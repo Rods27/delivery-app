9e04d975c87d1f829c6e6b99814507ad51b76b5c
Rodrigo Leite <rods.leite27@gmail.com>
2021-08-06 22:23:08 -0300

    port

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,6 +1,6 @@
 // const socket = require('socket.io');
 const app = require('./app');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Listening in on port: ${PORT}`));
