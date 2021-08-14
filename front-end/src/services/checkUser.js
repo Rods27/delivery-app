8d5443aa71a684c3ce77f2bb25109a3a0b167d7c
Rodrigo Leite <rods.leite27@gmail.com>
2021-08-14 14:47:40 -0300

    checkUser

--- front-end/src/services/checkUser.js
+++ front-end/src/services/checkUser.js
@@ -1,16 +1,20 @@
 export default function checkUser(history) {
   const token = JSON.parse(localStorage.getItem('user'));
   const { location: { pathname } } = history;
+  const alert = 'Usuário não autorizado.';
   if (token && token.token) {
     const { role } = token;
     if (role === 'customer' && !pathname.includes('/customer/')) {
       history.push('/customer/products');
+      window.alert(alert);
     }
     if (role === 'seller' && !pathname.includes('/seller/')) {
       history.push('/seller/orders');
+      window.alert(alert);
     }
     if (role === 'administrator' && pathname !== '/admin/manage') {
       history.push('/admin/manage');
+      window.alert(alert);
     }
   } else {
     localStorage.setItem('user', JSON.stringify({}));
